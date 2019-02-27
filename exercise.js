2a252b7b6968ae6210d05bf2496b3141f40aa0a1
YuQiuuuuu <yuqiu@mit.edu>
2019-02-26 19:56:06 -0500

    exercise updated

--- exercise.js
+++ exercise.js
@@ -1,37 +1,51 @@
 var exercise = {};
 
 exercise.countRecords = function(data){
-    //-------------------------------------------
-    // YOUR CODE
-    // Return the total number of records
-    //-------------------------------------------
+    var num = 0;
+    var add =  function(item,index,array){
+        num = num + 1;
+    }
+    data.forEach(add)
+    return num
     return 'Error: countRecords not implemented'; 
 };
 
 exercise.countDistrictCrimes = function(data,district){
-    //-------------------------------------------
-    // YOUR CODE
-    // Return the number of crimes 
-    // for the given district
-    //-------------------------------------------
+    var NumCrimeDistrict = 0;
+    var add2 = function(item,index,array){
+        // console.log(item[19])
+        if (item[19] == district){
+            NumCrimeDistrict = NumCrimeDistrict + 1;
+        }
+    }
+    data.forEach(add2)
+    return NumCrimeDistrict
     return 'Error: countDistrictCrimes not implemented'; 
 };
 
 exercise.countPrimaryType = function(data,primaryType){
-    //-------------------------------------------
-    // YOUR CODE
-    // Return the number of crimes 
-    // for the giving primary type
-    //-------------------------------------------
+    var NumPrimaryType = 0;
+    var add3 = function(item,index,array){
+        // console.log(item[13])
+        if (item[13] == primaryType){
+            NumPrimaryType = NumPrimaryType + 1;
+        }
+    }
+    data.forEach(add3)
+    return  NumPrimaryType
     return 'Error: countPrimaryType not implemented';
 };
 
 exercise.countLocation = function(data,location){
-    //-------------------------------------------
-    // YOUR CODE
-    // Return the number of crimes 
-    // for the given location
-    //-------------------------------------------
+    var NumLocation = 0;
+    var add3 = function(item,index,array){
+        // console.log(item[15])
+        if (item[15] == location){
+            NumLocation = NumLocation + 1;
+        }
+    }
+    data.forEach(add3)
+    return  NumLocation
     return 'Error: countLocation not implemented';
 };
 
@@ -43,10 +57,11 @@ exercise.buildLatLngPoint = function(crime){
     // 
     // For example:
     //
-    // var point = {};    
-    // point.latitude = crime[latitudeColumnNumber];
-    // point.longitude = crime[longitudeColumnNumber];
-    // return point;    
+    var point = {};    
+    point.latitude = crime[26];
+    point.longitude = crime[28];
+    // console.log(point)
+    return point;    
     //-------------------------------------------    
     return 'Error: buildLatLngPoint not implemented';
 };
